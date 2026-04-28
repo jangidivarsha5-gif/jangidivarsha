7ee7b0bbee4c50b22a7bbe38fca098a31587506a
jangidivarsha5-gif <jangidivarsha5@gmail.com>
2026-04-28 20:28:41 +0530

    Add files via upload

---
+++ calendar2.py
@@ -0,0 +1,5 @@
+import calender
+a = int(input("Enter any year: "))
+b = int(input("Enter any month: "))
+res = calendar.month(a,b)
+print(res)
---
+++ no.of days.py
@@ -0,0 +1,4 @@
+from datetime import date
+a = date (2005,10,14)
+b = date (2026,4,28)
+print("no of days she lived: ",(b-a).total_seconds()/3600)
